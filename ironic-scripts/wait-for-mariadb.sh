17000d6befc528176e5659f491d3537941e0cc78
jdlugole <jakub.dlugolecki@gmail.com>
2020-02-26 07:19:35 +0000

    Print log messages from the conductor init container

--- ironic-scripts/wait-for-mariadb.sh
+++ ironic-scripts/wait-for-mariadb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while ! mysql -u "${DB_USER}" --password="${DB_PASS}" -h "${DB_NAME}" -e 'show databases;' >/dev/null 2>&1
+while ! mysql -u "${DB_USER}" --password="${DB_PASS}" -h "${DB_NAME}" -e 'show databases;'
 do
 	sleep 1
 	echo -n "."
